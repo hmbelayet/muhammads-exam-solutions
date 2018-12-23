3c5edae4e558791290f8f947b3a02841a10c4d84
MUHAMMAD HOSSAIN <belayet@MUHAMMADs-MacBook-Pro.local>
2018-12-23 11:54:09 -0500

    first commit

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -2,10 +2,18 @@ package math.problems;
 
 public class Fibonacci {
     public static void main(String[] args) {
-         /*
-          Write 40 Fibonacci numbers with java.
-         */
-
+/*
+Write 40 Fibonacci numbers with java.
+*/
+        int n, a = 0, b = 0, c = 1;
+        System.out.print("40 Fibonacci numbers: ");
+        for(int i = 1; i <= 40; i++)
+        {
+            a = b;
+            b = c;
+            c = a + b;
+            System.out.print(a+" ");
+        }
 
     }
 }
