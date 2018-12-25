8dfae19222c71e335fb83d183de7ff809b4aa004
MUHAMMAD HOSSAIN <belayet@MUHAMMADs-MacBook-Pro.local>
2018-12-25 13:06:38 -0500

    math.problems

--- src/math/problems/LowestNumber.java
+++ src/math/problems/LowestNumber.java
@@ -1,3 +1,4 @@
+
 package math.problems;
 
 import databases.ConnectToSqlDB;
@@ -6,17 +7,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LowestNumber {
+	public static int findLowestNum(int[] array){
+		int min = array[0];
+		for (int i = 1; i< array.length;i++){
+			if (array[i] < min) min = array[i];
+		}
+		System.out.println(min);
+		return min;
+	}
+
 
 	public static void main(String[] args) {
-
 		/*
 		 * Write java solution to find the lowest number from this array.
-
 		 * Use one of the databases from mysql or mongodb to store and to retrieve.
 		 */
-		int array[] = new int[]{211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123, 67, 5, 679, 54, 32, 65};
+		int  array[] = new int[]{211,110,99,34,67,89,67,456,321,456,78,90,45,32,56,78,90,54,32,123,67,5,679,54,32,65};
 
-		//find lowest number from the array
+		//find lowestNum from the array;
+
+
+		findLowestNum(array);
+
 
 		ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
 		List<String> lowestValue = new ArrayList<String>();
@@ -28,9 +40,10 @@ public class LowestNumber {
 			e.printStackTrace();
 		}
 		System.out.println("Data is reading from the Table (tbl_primenumber) and displaying to the console");
-		for (String st : lowestValue) {
+		for(String st:lowestValue){
 			System.out.println(st);
 		}
 	}
 
 }
+
