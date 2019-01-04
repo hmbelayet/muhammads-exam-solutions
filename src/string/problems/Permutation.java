e81c048c3b0ec1b300e4a183f48a3278f54252ce
MUHAMMAD HOSSAIN <belayet@MUHAMMADs-MacBook-Pro.local>
2019-01-04 03:37:55 -0500

    String.problem

--- src/string/problems/Permutation.java
+++ src/string/problems/Permutation.java
@@ -6,11 +6,25 @@ package string.problems;
 public class Permutation {
 
     public static void main(String[] args) {
-
         /*
          * Permutation of String "ABC" is "ABC" "BAC" "BCA" "ACB" "CAB" "CBA".
          * Write Java program to compute all Permutation of a String
          *
          */
+        permutation("123");
+    }
+
+    public static void permutation(String input) {
+        permutation("", input);
+
+    }
+    private static void permutation(String perm, String word) {
+        if(word.isEmpty()) {
+            System.err.println(perm + word);
+        } else {
+            for (int i = 0; i<word.length(); i++) {
+                permutation(perm + word.charAt(i), word.substring(0,i) + word.substring(i + 1, word.length()));
+            }
+        }
     }
 }
